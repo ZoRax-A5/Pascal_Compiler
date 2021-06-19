085a9c4678ea60bd67e527b9d16888ed412a6fe5
xiaotiancai <592248609@qq.com>
2021-06-19 09:13:55 -0700

    update compiler v0.0

--- test/easy.pas
+++ test/easy.pas
@@ -4,10 +4,16 @@ var
     b : real;
     c : char;
     d : boolean;
+    e : real;
 begin
     { this is a comment }
-    a := 3;
+    a := -3;
     b := 3.2;
     c := 'c';
     d := true;
+    e := a+b;
+    e := 3+b;
+    e := 3+3.2;
+    e := a+b+3;
+    e := a*b;
 end.
